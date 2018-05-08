a35e5dc8120246979b690541e8fd17afc45fe824
gongxiaobo <15828021872@163.com>
2018-05-08 17:42:12 +0800

    test
    test

--- miniGame/Assets/Script/NewBehaviourScript.cs
+++ miniGame/Assets/Script/NewBehaviourScript.cs
@@ -8,11 +8,10 @@ public class NewBehaviourScript : MonoBehaviour {
 	void Start () {
           
           Debug.Log("<color=blue>blue:</color>");
-<<<<<<< HEAD
+
           
           Debug.Log("<color=red>red:</color>"+"ddd");
-=======
->>>>>>> parent of 37287bd... test
+
      
 	}
 	
