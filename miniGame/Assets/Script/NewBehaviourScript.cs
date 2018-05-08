cb8ba2a61ffcff2c9d71359ca4ce7dfe6e12df5d
gongxiaobo <15828021872@163.com>
2018-05-08 17:57:37 +0800

    test
    test

--- miniGame/Assets/Script/NewBehaviourScript.cs
+++ miniGame/Assets/Script/NewBehaviourScript.cs
@@ -11,7 +11,7 @@ public class NewBehaviourScript : MonoBehaviour {
 
           
 
-          Debug.Log("<color=red>red:</color>"+"ddd");
+          Debug.Log("<color=red>red:</color>"+"dddy");
 
 
      
