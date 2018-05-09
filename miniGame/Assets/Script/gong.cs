520a181dad5e158473b861410047ad925ec929f7
damimi1024 <33301027+damimi1024@users.noreply.github.com>
2018-05-09 17:33:51 +0800

    11

--- miniGame/Assets/Script/gong.cs
+++ miniGame/Assets/Script/gong.cs
@@ -4,9 +4,10 @@ using UnityEngine;
 
 public class gong : MonoBehaviour {
 
+    private test_2 test;
 	// Use this for initialization
 	void Start () {
-		
+        test.enabled = false;
 	}
 	
 	// Update is called once per frame
