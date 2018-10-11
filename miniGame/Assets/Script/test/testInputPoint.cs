96b0057109a60ce88d65429bc092fe8cb324a7ed
gongxiaobo <15828021872@163.com>
2018-10-11 11:28:07 +0800

    动态生成模型的功能导入，使用的是bezier曲线

---
+++ miniGame/Assets/Script/test/testInputPoint.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class testInputPoint : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+
+          if (Input.GetMouseButtonDown(0))
+          {
+               //从屏幕空间点击，发射射线到3d空间中
+
+               Debug.Log("<color=red>red:</color>" + Input.mousePosition);
+     
+               Ray t_cameraRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+               RaycastHit t_rayhit;
+               if (Physics.Raycast(t_cameraRay, out t_rayhit))
+               {
+                    Vector3 t_hitPoint = t_rayhit.point;
+                    Debug.Log("<color=blue>blue:</color>" + t_hitPoint);
+                    
+               }
+
+          }
+		
+	}
+}
---
+++ miniGame/Assets/Script/test/test_rotate.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class test_rotate : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+          //transform.rotation = Quaternion.AngleAxis(30, Vector3.up);
+	}
+     public float m_angle = 10f;
+	// Update is called once per frame
+	void Update () {
+          if (Input.GetKeyDown(KeyCode.D))
+          {
+               transform.rotation = Quaternion.AngleAxis(m_angle, Vector3.up);
+          }
+          
+	}
+}
